3bdd473ee241461e5a158b669241d2bf1b5a99b0
nandhu-44 <nandhu90040@gmail.com>
2023-01-09 20:07:53 +0530

    Almost entire raptor files are written in python

---
+++ n-odd-numbers/main.py
@@ -0,0 +1,3 @@
+terms = int(input("Enter the no of terms : "))
+for i in range(1, terms*2+1,2):
+    print(i)
---
+++ n-terms-of-ap/main.py
@@ -0,0 +1,6 @@
+first_term = int(input("Enter the value of first term (a) : "))
+diff = int(input("Enter the value of common difference (d) : "))
+num = int(input("Enter the number of terms (n) : "))
+for i in range(1, num+1):
+    print(first_term)
+    first_term += diff
